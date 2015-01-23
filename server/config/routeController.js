55232f61fbde4dbf3447236f1ab43690dcc92469
Hack Reactor Students <accounts@hackreactor.com>
2015-01-22 20:24:59 -0800

    Set up basic boilerplate

---
+++ server/config/routeController.js
@@ -0,0 +1,17 @@
+//declare a router
+var controller = {};
+
+//set up controllers for routes
+//for example
+/*
+controller.items = {};
+controller.items.addItem = function(req,res,next){
+
+}
+
+
+
+*/
+
+
+module.exports = controller;

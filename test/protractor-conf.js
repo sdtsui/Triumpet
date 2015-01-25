8150db4dd744001ceeb073707834681ee7cb5fad
sdtsui <danielt213@gmail.com>
2015-01-25 02:53:52 -0800

    testing scaffold

---
+++ test/protractor-conf.js
@@ -0,0 +1,9 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: [
+  './Client/**/*.js',
+  './Server/**/*.js',
+  './e2e/**/*.js'
+  ],
+  baseUrl: 'https://localhost:????'
+};

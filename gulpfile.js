77d451da1003323a0397fac96f8a083d23600dee
sdtsui <danielt213@gmail.com>
2015-01-25 02:58:01 -0800

    small changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-gulpfile.js
+var protractor = require("gulp-protractor").protractor;
+var webdriver_standalone = require("gulp-protractor").webdriver_standalone;
+gulp.task('webdriver_standalone', webdriver_standalone);
+
+gulp.src(["./src/tests/*.js"])
+    .pipe(protractor({
+        configFile: "test/protractor.config.js",
+        args: ['--baseUrl', 'http://127.0.0.1:8000']
+    })) 
+    .on('error', function(e) { throw e })

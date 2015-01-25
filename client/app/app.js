3fe82a2c30715b52f8378f4a095edaadf9c3e3b6
Devin Otway <devin.otway@gmail.com>
2015-01-24 16:03:48 -0800

    adds controllers to app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,15 @@
 //Main app factories
+var app = angular.module('trumpet', []);
+
+app.controller('MainCtrl', function($scope){
+
+});
+
+app.controller('ProfileCtrl', function(){
+
+});
+
+
+app.controller('searchCtrl', function(){
+
+});

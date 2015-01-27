694eaaab226977169d27c8d28d745e7e50ee297b
Devin Otway <devin.otway@gmail.com>
2015-01-26 16:49:56 -0800

    deletes whitespace

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,6 @@
 'ues strict';
 
-
 var app = angular.module('trumpet', []);
-
 
 // configures routes for the app
 app.config(function($routeProvider){
@@ -12,35 +10,13 @@ app.config(function($routeProvider){
   });
 });
 
-
 // this directive will be for rendering the svg map using d3 and updating it as needed
 app.directive('tpMapViz', function(){
-  // define svg constants here, width/height
-  
+  // define svg constants here, width/height 
   return {
     restrict: 'AE',
-    
-
   },
   link: function(scope, element, attrs) {
 
   }
-
-
-
 });
-
-
-
-
-
-
-// app.config(function($routeProvider){
-//   $routeProvider.when("/",
-//     {
-//       templateUrl: "app.html",
-//       controller: "AppCtrl"
-//     }
-//   );
-// });
-

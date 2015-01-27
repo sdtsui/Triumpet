c8572c9255a5f1b1a8857725aef3a12edd42ad8e
Devin Otway <devin.otway@gmail.com>
2015-01-26 17:17:51 -0800

    adds getItems function to mapCtrl

--- client/app/map/mapCtrl.js
+++ client/app/map/mapCtrl.js
@@ -1,12 +1,22 @@
 angular.module('tp.map',[])
 
-.controller('MapCtrl', function($scope){
+.controller('MapCtrl', function($scope, $http){
+  $scope.serverSettings = serverSettings = {
+    // this will need to be updated to be production ready
+    url: 'http://localhost:8080'
+  };
   $scope.userLoc;
   $scope.items;
 
   //needs to be able to fetch the items from the database and cache them in items
   $scope.getItems = function(){
-    
+    $http.get(serverSettings.url + '/api/items/:retailer'). // this will need to be updated to whatever the actual retailer is...
+      success(function(data, status){
+        $scope.items = data;
+      }).
+      error(function(data, status){
+        console.error('[Error]: While Attempting to Fetch Items');
+      });
   };
+});
 
-})

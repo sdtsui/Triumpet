2741124754886811cd9ba47bb7ce9101a2ca8f16
chikeichan <chikeichan@localhost.(none)>
2015-01-26 22:39:41 -0800

    Finished Signup View

--- client/app/app.js
+++ client/app/app.js
@@ -5,22 +5,25 @@ angular.module('triumpet', [
   'tp.auth',
   'tp.editor',
   'tp.map',
-  'ui.router'
+  'ui.router',
+  'ngRoute'
 ])
 
 // configures routes for the app
-.config(function($stateProvider){
+.config(function($stateProvider, $httpProvider, $routeProvider){
   $stateProvider
+    .state('signup',{
+      url         : '/signup',
+      template    : '<tp-sign-up></tp-sign-up>',
+      controller  : 'AuthCtrl'
+    })
     .state('home',{
       url         : '/',
       templateUrl : "",
       controller  : ""
-    })
-    .state('home.signup',{
-      url         : '/signup',
-      tempalte    : '',
-      controller  : ''
-    })
+    });
+
+    $httpProvider.interceptors.push('AttachTokens');
 })
 
 // this directive will be for rendering the svg map using d3 and updating it as needed
@@ -32,4 +35,28 @@ angular.module('triumpet', [
 
     }
   }
-});
+})
+
+.factory('AttachTokens',function($window){
+  var attach = {
+    request: function(object){
+      var jwt = $window.localStorage.getItem('com.triumpet.token');
+      if(jwt){
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+
+.run(function($rootScope, $location, Auth){
+  //TODO: Code to verify token. Uncomment after signin page is complete.
+
+  // $rootscope.on($routeChangeStart, function(evt, next, current){
+  //   if(next.$$route && next.$$route.authenticate && !Auth.isAuth()){
+  //     $location.path('/signin');
+  //   }
+  // })
+})

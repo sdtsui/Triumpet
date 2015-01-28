db28b655e9b7797990536962105ae89d45b2e8c5
Hack Reactor Students <accounts@hackreactor.com>
2015-01-27 17:02:10 -0800

    eslint working

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
- console.log('Triumpet.');
+gulp.task('lint', function () {
+    // Note: To have the process exit with an error code (1) on
+    //  lint error, return the stream and pipe to failOnError last.
+    return gulp.src(['client/**/*.js', '!client/project/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+  });
+
+gulp.task('default', ['lint'], function () {
+    console.log('Linted.');
 });

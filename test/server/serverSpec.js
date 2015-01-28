56f341b1e20e6e81cae150fecda17305397e71fe
Hack Reactor Students <accounts@hackreactor.com>
2015-01-28 03:48:19 -0800

    set up chaiAsPromised, about to play around: H/R Maintainance

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -1,5 +1,10 @@
 var chai = require('chai');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
 var expect = chai.expect;
+var should = chai.should;
 
 describe('serverSpec', function(){
   it('should begin server tests', function(){

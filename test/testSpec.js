3a041bf8b98d42cdc1f1cd4659999c517ef4e939
Hack Reactor Students <accounts@hackreactor.com>
2015-01-29 01:11:39 -0800

    removed unit/server folder, no server unit tests. OPEN ISSUE

--- test/testSpec.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai');
-var expect = chai.expect;
-
-describe('dummy test spec', function(){
-  it ('should demonstrate mocha is running tests', function(){
-    expect('dummy test').to.be.a('string');
-  });
-});
--- test/unit/server/serverSpec.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai');
-var expect = chai.expect;
-
-xdescribe('Dummy Server Test', function(){
-  it ('should run server tests', function(){
-    expect('Open Issue: Server Unit tests!').to.be.a('string');
-  });
-});

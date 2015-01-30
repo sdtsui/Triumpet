90829cbf3fbbde2f0268dfec46b9913b817de0a0
sdtsui <danielt213@gmail.com>
2015-01-29 22:23:47 -0800

    added Protractor+Webdriver

---
+++ test/e2e/basicBrowserTest.js
@@ -0,0 +1,18 @@
+// var protractor = require('protractor');
+// var ptor = protractor.getInstance();
+
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', function() {
+    // browser.get('http://www.angularjs.org');
+    browser.get('http://localhost:8080/#/signup')
+    // // element(by.model('todoText'))#.sendKeys('write a protractor test');
+    // // element(by.css('[value="add"]')).click();
+
+    // // var todoList = element.all(by.repeater('todo in todos'));
+    // expect(todoList.count()).toEqual(3);
+    // expect(todoList.get(2).getText()).toEqual('write a protractor test');
+
+    expect(browser.getTitle()).toEqual('Triumpet');
+    console.log('end of test');
+  });
+});

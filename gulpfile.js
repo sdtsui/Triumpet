f4bf6adb24bacff1b24ca43d4b4033482d98c967
sdtsui <danielt213@gmail.com>
2015-01-30 17:23:29 -0800

    rearranging protractor files for istanbul

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var shell = require('gulp-shell');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('lint', function() {
   return gulp.src([
@@ -16,6 +17,18 @@ gulp.task('lint', function() {
 gulp.task('default', ['lint'], function () {
     console.log('Linted.');
 });
+
+// gulp.task('coverTest', function (cb) {
+//   gulp.src(['server/**/*.js', 'server.js'])
+//     .pipe(istanbul()) // Covering files
+//     .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+//     .on('finish', function () {
+//       gulp.src(['test/*.js'])
+//         .pipe(mocha())
+//         .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+//         .on('end', cb);
+//     });
+// });
 
 gulp.task('test', shell.task([
   'mocha -R spec --recursive'
